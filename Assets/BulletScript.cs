103753d3a53ae328fae5fe8ee2f5ca884e8dc8c8
CiprianPintilie <pintilie.c.a@gmail.com>
2017-12-16 18:40:50 +0100

    Change - Player scripts

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float Speed;
+    public float Damage;
+
+    private Transform _bulletTransform;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _bulletTransform = gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_bulletTransform.Translate(_bulletTransform.right * Speed * Time.deltaTime);
+	}
+}
--- Assets/PlayerControlsScript.cs
+++ Assets/PlayerControlsScript.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerControlsScript : MonoBehaviour
 {
@@ -7,32 +6,45 @@ public class PlayerControlsScript : MonoBehaviour
     public float Speed;
     public float JumpForce;
     public float FallForce;
+    public GameObject[] Weapons;
+    public int CurrentWeapon;
 
     private Transform _playerTransform;
     private Rigidbody2D _playerRigidBody;
     private bool _isGrounded;
     private bool _jumpOrder;
+    private bool _shootOrder;
+    private GameObject _barrel;
 
     // Use this for initialization
     void Start()
     {
         _playerTransform = gameObject.transform;
         _playerRigidBody = gameObject.GetComponent<Rigidbody2D>();
+        _barrel = gameObject.transform.GetChild(0).gameObject;
         _isGrounded = true;
+        CurrentWeapon = 0;
     }
 
     void Update()
     {
         if (Input.GetButtonDown("Jump") && _isGrounded)
             _jumpOrder = true;
+        if (Input.GetButtonDown("Fire1"))
+            _shootOrder = true;
+        if (Input.GetButtonUp("Fire1"))
+            _shootOrder = false;
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetButton("Horizontal"))
+        _playerTransform.Translate(_playerTransform.right * Input.GetAxis("Horizontal") * Speed);
+
+        if (_shootOrder)
         {
-            _playerTransform.Translate(_playerTransform.right * Input.GetAxis("Horizontal") * Speed);
+            Instantiate(Weapons[CurrentWeapon], _barrel.transform.position, _barrel.transform.rotation);
         }
         if (_jumpOrder)
         {
@@ -40,9 +52,7 @@ public class PlayerControlsScript : MonoBehaviour
             _jumpOrder = false;
         }
         if (_playerRigidBody.velocity.y < 0 && !_isGrounded)
-        {
             _playerRigidBody.velocity += Vector2.up * Physics.gravity.y * FallForce;
-        }
     }
 
     private void OnTriggerExit2D(Collider2D collider)

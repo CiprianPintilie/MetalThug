c46c614e1dc86cf01f876378e4e83168c5adacaa
CiprianPintilie <pintilie.c.a@gmail.com>
2017-12-18 21:22:27 +0100

    New - Add camera movement when the player moves right beyond the camera's center

---
+++ Assets/MainCameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraScript : MonoBehaviour
+{
+
+    public float CameraSpeed;
+    
+    private Transform _playerTransform;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _playerTransform = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (transform.localPosition.x < _playerTransform.position.x && Input.GetAxis("Horizontal") > 0)
+	        transform.Translate(transform.right * Time.deltaTime * CameraSpeed);
+	}
+}

b61019a22b87415c85ffa6b712b6b13091883819
CiprianPintilie <pintilie.c.a@gmail.com>
2017-12-21 22:19:45 +0100

    fdkhsdfuyfg

--- Assets/Scripts/Ennemy/MovingEnnemyScript.cs
+++ Assets/Scripts/Ennemy/MovingEnnemyScript.cs
@@ -8,6 +8,8 @@ public class MovingEnnemyScript : MonoBehaviour
     private GameObject _objectToFollow;
     private Transform _enemyTransform;
     private Animator _enemyAnimator;
+    private Renderer _enemyRenderer;
+    private Transform _playerTransform;
 
     // Use this for initialization
     void Start ()
@@ -15,12 +17,16 @@ public class MovingEnnemyScript : MonoBehaviour
 	    _enemyTransform = gameObject.transform;
         _objectToFollow = GameObject.FindWithTag("Player");
 	    _enemyAnimator = gameObject.GetComponent<Animator>();
-    }
+	    _enemyRenderer = gameObject.GetComponent<Renderer>();
+	    _playerTransform = GameObject.FindWithTag("Player").transform;
+
+	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	    SetDirection();
+	    if (!_enemyRenderer.isVisible && transform.position.x > _playerTransform.position.x) return;
+        SetDirection();
 	    _enemyAnimator.SetInteger("State", Direction);
 	    if (_objectToFollow != null && _objectToFollow.transform.localPosition.y < _enemyTransform.position.y + 2)
             _enemyTransform.Translate(_enemyTransform.right * Speed * Direction * Time.deltaTime);
